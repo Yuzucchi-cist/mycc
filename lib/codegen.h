7db79e489aea3e9675e84c12c1f3832bccbd494c
yuzuchi <yuzucchi.cist@gmail.com>
2024-09-11 09:56:25 +0900

    fix duplicated declaration and add grammar comments

--- lib/codegen.h
+++ lib/codegen.h
@@ -30,22 +30,25 @@ node_t *new_node(NodeKind kind, node_t *lhs, node_t *rhs);
 
 node_t *new_node_num(int val);
 
-node_t *mul();
-node_t *primary();
-node_t *unary();
-
+// expr = equality
 node_t *expr();
 
+// equality = relational ("==" relational | "!=" relational)*
 node_t *equality();
 
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
 node_t *relational();
 
+// add = mul ("+" mul | "-" mul)*
 node_t *add();
 
+// mul = unary ("*" unary | "/" unary)*
 node_t *mul();
 
+// unary = ("+" | "-")? primary
 node_t *unary();
 
+// primary = num | "(" expr ")"
 node_t *primary();
 
 void gen(node_t *node);
